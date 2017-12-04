87b59c72c1a572a337daae7315f320363cfe6f3e
unknown <FH@EBP01041.de.ebpgroup.corp>
2017-12-04 22:25:38 +0100

    root project is now printed by tree

--- src/main/java/de/ebp/dependencymanagement/TreeMojo.java
+++ src/main/java/de/ebp/dependencymanagement/TreeMojo.java
@@ -4,13 +4,19 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.artifact.ProjectArtifact;
 
 @Mojo(name = "tree", requiresDependencyResolution = ResolutionScope.TEST)
 public class TreeMojo extends AbstractMojo {
 
+	@Parameter(defaultValue = "${project}", readonly = true, required = true)
+	private MavenProject project;
+
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().debug("Hello World");
+		getLog().info(new ProjectArtifact(project).toString());
 	}
 
 }
--- src/test/java/de/ebp/dependencymanagement/TreeMojoTest.java
+++ src/test/java/de/ebp/dependencymanagement/TreeMojoTest.java
@@ -2,12 +2,30 @@ package de.ebp.dependencymanagement;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.inOrder;
 
+import java.io.File;
+import java.util.Arrays;
+
+import org.apache.maven.DefaultMaven;
+import org.apache.maven.Maven;
+import org.apache.maven.execution.DefaultMavenExecutionRequest;
+import org.apache.maven.execution.DefaultMavenExecutionResult;
+import org.apache.maven.execution.MavenExecutionRequest;
+import org.apache.maven.execution.MavenExecutionRequestPopulator;
+import org.apache.maven.execution.MavenExecutionResult;
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.ProjectBuilder;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+import org.eclipse.aether.internal.impl.SimpleLocalRepositoryManagerFactory;
+import org.eclipse.aether.repository.LocalRepository;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -20,11 +38,67 @@ public class TreeMojoTest {
 	@Rule
 	public MojoRule rule = new MojoRule() {
 
+		protected MavenSession newMavenSession() {
+			try {
+				MavenExecutionRequest request = new DefaultMavenExecutionRequest();
+				MavenExecutionResult result = new DefaultMavenExecutionResult();
+
+				// populate sensible defaults, including repository basedir and
+				// remote repos
+				MavenExecutionRequestPopulator populator;
+				populator = getContainer().lookup(MavenExecutionRequestPopulator.class);
+				populator.populateDefaults(request);
+
+				// this is needed to allow java profiles to get resolved; i.e.
+				// avoid during project builds:
+				// [ERROR] Failed to determine Java version for profile
+				// java-1.5-detected @ org.apache.commons:commons-parent:22,
+				// /Users/alex/.m2/repository/org/apache/commons/commons-parent/22/commons-parent-22.pom,
+				// line 909, column 14
+				request.setSystemProperties(System.getProperties());
+
+				// and this is needed so that the repo session in the maven
+				// session
+				// has a repo manager, and it points at the local repo
+				// (cf MavenRepositorySystemUtils.newSession() which is what is
+				// otherwise done)
+				DefaultMaven maven = (DefaultMaven) getContainer().lookup(Maven.class);
+				DefaultRepositorySystemSession repoSession = (DefaultRepositorySystemSession) maven
+						.newRepositorySession(request);
+				repoSession.setLocalRepositoryManager(new SimpleLocalRepositoryManagerFactory().newInstance(repoSession,
+						new LocalRepository(request.getLocalRepository().getBasedir())));
+
+				@SuppressWarnings("deprecation")
+				MavenSession session = new MavenSession(getContainer(), repoSession, request, result);
+				return session;
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		/**
+		 * Extends the super to use the new {@link #newMavenSession()}
+		 * introduced here which sets the defaults one expects from maven; the
+		 * standard test case leaves a lot of things blank
+		 */
+		public MavenSession newMavenSession(MavenProject project) {
+			MavenSession session = newMavenSession();
+			session.setCurrentProject(project);
+			session.setProjects(Arrays.asList(project));
+			return session;
+		}
+
 		@Override
 		protected void before() throws Throwable {
 			MockitoAnnotations.initMocks(TreeMojoTest.this);
 
-			testedTreeMojo = (TreeMojo) rule.lookupMojo("tree", "src/test/resources/unit/tree-mojo/pom.xml");
+			ProjectBuildingRequest buildingRequest = newMavenSession().getProjectBuildingRequest();
+			ProjectBuilder projectBuilder = lookup(ProjectBuilder.class);
+			MavenProject project = projectBuilder
+					.build(new File("src/test/resources/unit/tree-mojo/pom.xml"), buildingRequest).getProject();
+
+			testedTreeMojo = (TreeMojo) lookupConfiguredMojo(project, "tree");
+
 			assertThat(testedTreeMojo, notNullValue());
 
 			testedTreeMojo.setLog(mockedLog);
@@ -39,7 +113,8 @@ public class TreeMojoTest {
 	public void testSomething() throws Exception {
 		testedTreeMojo.execute();
 
-		verify(mockedLog).debug("Hello World");
+		InOrder inOrder = inOrder(mockedLog);
+		inOrder.verify(mockedLog).info("de.ebp:tree-mojo-test:pom:0.0.1-SNAPSHOT");
 	}
 
 }
