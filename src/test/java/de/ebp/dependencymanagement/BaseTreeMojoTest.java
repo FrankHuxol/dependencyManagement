af5852fb25feb19450a08b53e9d04062601dd7eb
FrankHuxol <frank.huxol@ebp.de>
2022-03-09 10:05:17 +0100

    extracted setup functionality

--- src/test/java/de/ebp/dependencymanagement/BaseTreeMojoTest.java
+++ src/test/java/de/ebp/dependencymanagement/BaseTreeMojoTest.java
@@ -4,12 +4,21 @@ import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
 import org.apache.maven.artifact.repository.MavenArtifactRepository;
 import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.project.MavenProject;
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.io.File;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public abstract class BaseTreeMojoTest {
 
@@ -46,4 +55,31 @@ public abstract class BaseTreeMojoTest {
 
         );
     }
+
+    /**
+     * Reads a mojo for the prepared project
+     *
+     * @param projectFolder The project folder to read
+     * @param aGoal         The goal to read
+     * @return The mojo for the provided goal
+     * @throws Exception in case something goes wrong
+     */
+    protected AbstractMojo prepareMojo(String projectFolder, String aGoal) throws Exception {
+        MavenProject project = mojoRule.readMavenProject(new File(projectFolder));
+
+        // Generate session
+        MavenSession session = mojoRule.newMavenSession(project);
+
+        // add localRepo - framework doesn't do this on its own
+        ArtifactRepository localRepo = createLocalArtifactRepository();
+        session.getRequest().setLocalRepository(localRepo);
+
+        // Generate Execution and Mojo for testing
+        MojoExecution execution = mojoRule.newMojoExecution(aGoal);
+        AbstractMojo mojo = (AbstractMojo) mojoRule.lookupConfiguredMojo(session, execution);
+        assertThat(mojo, notNullValue());
+        mojo.setLog(mockedLog);
+        return mojo;
+    }
+
 }
--- src/test/java/de/ebp/dependencymanagement/TreeMojoMaxDepthTest.java
+++ src/test/java/de/ebp/dependencymanagement/TreeMojoMaxDepthTest.java
@@ -1,39 +1,17 @@
 package de.ebp.dependencymanagement;
 
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.MojoExecution;
-import org.apache.maven.project.MavenProject;
 import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
-import java.io.File;
-
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.*;
 
 public class TreeMojoMaxDepthTest extends BaseTreeMojoTest {
 
-    TreeMojo testedTreeMojo;
-
     @Test
     public void testTreeDepth1() throws Exception {
-        MavenProject project = mojoRule.readMavenProject(new File("src/test/resources/unit/tree-mojo/depth1"));
-
-        // Generate session
-        MavenSession session = mojoRule.newMavenSession(project);
-
-        // add localRepo - framework doesn't do this on its own
-        ArtifactRepository localRepo = createLocalArtifactRepository();
-        session.getRequest().setLocalRepository(localRepo);
-
-        // Generate Execution and Mojo for testing
-        MojoExecution execution = mojoRule.newMojoExecution("tree");
-        testedTreeMojo = (TreeMojo) mojoRule.lookupConfiguredMojo(session, execution);
-        assertThat(testedTreeMojo, notNullValue());
-        testedTreeMojo.setLog(mockedLog);
+        TreeMojo testedTreeMojo = (TreeMojo) prepareMojo("src/test/resources/unit/tree-mojo/depth1", "tree");
 
         // run test
         testedTreeMojo.execute();
