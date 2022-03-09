cc355022e11d7b235a72ac6062c242183afcc468
FrankHuxol <frank.huxol@ebp.de>
2022-03-09 20:36:51 +0100

    added depth4 test

--- src/test/java/de/ebp/dependencymanagement/TreeMojoMaxDepthTest.java
+++ src/test/java/de/ebp/dependencymanagement/TreeMojoMaxDepthTest.java
@@ -90,4 +90,40 @@ public class TreeMojoMaxDepthTest extends BaseTreeMojoTest {
             inOrder.verifyNoMoreInteractions();
         });
     }
+
+    @Test
+    public void testTreeDepth4() throws Exception {
+        TreeMojo testedTreeMojo = (TreeMojo) prepareMojo("src/test/resources/unit/tree-mojo/depth4", "tree");
+
+        // run test
+        testedTreeMojo.execute();
+
+        // verify
+        verify(inOrder -> {
+            // actually the very same as 3
+            inOrder.verify(mockedLog).info("Dependency tree of dependencymanagement configuration:");
+            inOrder.verify(mockedLog).info("de.ebp:tree-mojo-test-depth4:pom:0.0.1-SNAPSHOT");
+            inOrder.verify(mockedLog).info("+- tech.units:indriya:jar:2.1.3:compile");
+            inOrder.verify(mockedLog).info("|  +- javax.measure:unit-api:jar:2.1.3:compile");
+            inOrder.verify(mockedLog).info("|  +- tech.uom.lib:uom-lib-common:jar:2.1:compile");
+            inOrder.verify(mockedLog).info("|  +- javax.inject:javax.inject:jar:1:compile");
+            inOrder.verify(mockedLog).info("|  +- org.apiguardian:apiguardian-api:jar:1.1.1:compile");
+            inOrder.verify(mockedLog).info("|  +- org.osgi:org.osgi.annotation:jar:6.0.0:provided");
+            inOrder.verify(mockedLog).info("|  +- org.osgi:org.osgi.compendium:jar:5.0.0:provided");
+            inOrder.verify(mockedLog).info("|  +- org.osgi:org.osgi.core:jar:5.0.0:provided");
+            inOrder.verify(mockedLog).info("|  +- org.hamcrest:hamcrest-library:jar:2.2:test");
+            inOrder.verify(mockedLog).info("|  +- org.junit.jupiter:junit-jupiter-api:jar:5.7.1:test");
+            inOrder.verify(mockedLog).info("|  +- org.junit.jupiter:junit-jupiter-params:jar:5.7.1:test");
+            inOrder.verify(mockedLog).info("|  +- org.junit.jupiter:junit-jupiter-engine:jar:5.7.1:test");
+            inOrder.verify(mockedLog).info("|  +- org.junit.platform:junit-platform-launcher:jar:1.7.1:test");
+            inOrder.verify(mockedLog).info("|  \\- org.junit.platform:junit-platform-runner:jar:1.7.1:test");
+            inOrder.verify(mockedLog).info("+- org.slf4j:slf4j-api:jar:1.7.36:compile");
+            inOrder.verify(mockedLog).info("|  \\- junit:junit:jar:4.13:test");
+            inOrder.verify(mockedLog).info("\\- junit:junit:jar:4.13.2:test");
+            inOrder.verify(mockedLog).info("   +- org.hamcrest:hamcrest-core:jar:1.3:compile");
+            inOrder.verify(mockedLog).info("   \\- org.hamcrest:hamcrest-library:jar:1.3:test");
+            inOrder.verify(mockedLog).info("      \\- org.hamcrest:hamcrest-core:jar:1.3:compile");
+            inOrder.verifyNoMoreInteractions();
+        });
+    }
 }
