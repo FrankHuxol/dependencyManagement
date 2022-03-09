669ad59c3efb31711d5c42abc13f215028b8938c
FrankHuxol <frank.huxol@ebp.de>
2022-03-09 21:48:06 +0100

    added second test

--- src/test/java/de/ebp/dependencymanagement/TreeMojoRestrictScopeTest.java
+++ src/test/java/de/ebp/dependencymanagement/TreeMojoRestrictScopeTest.java
@@ -24,4 +24,38 @@ public class TreeMojoRestrictScopeTest extends BaseTreeMojoTest {
             inOrder.verifyNoMoreInteractions();
         });
     }
+
+    @Test
+    public void testCompileAndTestScope() throws Exception {
+        TreeMojo testedTreeMojo = (TreeMojo) prepareMojo("src/test/resources/unit/tree-mojo/compileAndTest", "tree");
+
+        // run test
+        testedTreeMojo.execute();
+
+        // verify
+        verify(inOrder -> {
+            // actually the very same as 3
+            inOrder.verify(mockedLog).info("Dependency tree of dependencymanagement configuration:");
+            inOrder.verify(mockedLog).info("de.ebp:tree-mojo-test-compileAndTest:pom:0.0.1-SNAPSHOT");
+            inOrder.verify(mockedLog).info("+- tech.units:indriya:jar:2.1.3:compile");
+            inOrder.verify(mockedLog).info("|  +- javax.measure:unit-api:jar:2.1.3:compile");
+            inOrder.verify(mockedLog).info("|  +- tech.uom.lib:uom-lib-common:jar:2.1:compile");
+            inOrder.verify(mockedLog).info("|  +- javax.inject:javax.inject:jar:1:compile");
+            inOrder.verify(mockedLog).info("|  +- org.apiguardian:apiguardian-api:jar:1.1.1:compile");
+            inOrder.verify(mockedLog).info("|  +- org.hamcrest:hamcrest-library:jar:2.2:test");
+            inOrder.verify(mockedLog).info("|  +- org.junit.jupiter:junit-jupiter-api:jar:5.7.1:test");
+            inOrder.verify(mockedLog).info("|  +- org.junit.jupiter:junit-jupiter-params:jar:5.7.1:test");
+            inOrder.verify(mockedLog).info("|  +- org.junit.jupiter:junit-jupiter-engine:jar:5.7.1:test");
+            inOrder.verify(mockedLog).info("|  +- org.junit.platform:junit-platform-launcher:jar:1.7.1:test");
+            inOrder.verify(mockedLog).info("|  \\- org.junit.platform:junit-platform-runner:jar:1.7.1:test");
+            inOrder.verify(mockedLog).info("+- org.slf4j:slf4j-api:jar:1.7.36:compile");
+            inOrder.verify(mockedLog).info("|  \\- junit:junit:jar:4.13:test");
+            inOrder.verify(mockedLog).info("\\- junit:junit:jar:4.13.2:test");
+            inOrder.verify(mockedLog).info("   +- org.hamcrest:hamcrest-core:jar:1.3:compile");
+            inOrder.verify(mockedLog).info("   \\- org.hamcrest:hamcrest-library:jar:1.3:test");
+            inOrder.verify(mockedLog).info("      \\- org.hamcrest:hamcrest-core:jar:1.3:compile");
+            inOrder.verifyNoMoreInteractions();
+        });
+    }
+
 }
