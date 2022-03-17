441280ae37e24efb3e8e2e9047cac967f5f918e7
FrankHuxol <frank.huxol@ebp.de>
2022-03-17 16:49:09 +0100

    updated tests

--- src/test/java/de/ebp/dependencymanagement/TreeMojoExclusionsTest.java
+++ src/test/java/de/ebp/dependencymanagement/TreeMojoExclusionsTest.java
@@ -16,14 +16,9 @@ public class TreeMojoExclusionsTest extends BaseTreeMojoTest {
             inOrder.verify(mockedLog).info("Gathering dependency tree of dependencyManagement section. May take a while");
             inOrder.verify(mockedLog).info("Dependency tree of dependencymanagement configuration:");
             inOrder.verify(mockedLog).info("de.ebp:tree-mojo-test-exclusions-direct:pom:0.0.1-SNAPSHOT");
-            inOrder.verify(mockedLog).info("\\- com.google.guava:guava:jar:31.1-jre:compile");
+            inOrder.verify(mockedLog).info("\\- com.google.dagger:dagger:jar:2.41:compile");
             // this SHOULD be missing
-            // inOrder.verify(mockedLog).info("   +- com.google.guava:failureaccess:jar:1.0.1:compile");
-            inOrder.verify(mockedLog).info("   +- com.google.guava:listenablefuture:jar:9999.0-empty-to-avoid-conflict-with-guava:compile");
-            inOrder.verify(mockedLog).info("   +- com.google.code.findbugs:jsr305:jar:3.0.2:compile");
-            inOrder.verify(mockedLog).info("   +- org.checkerframework:checker-qual:jar:3.12.0:compile");
-            inOrder.verify(mockedLog).info("   +- com.google.errorprone:error_prone_annotations:jar:2.11.0:compile");
-            inOrder.verify(mockedLog).info("   \\- com.google.j2objc:j2objc-annotations:jar:1.3:compile");
+            // inOrder.verify(mockedLog).info("   \\- javax.inject:javax.inject:jar:1:compile");
             inOrder.verifyNoMoreInteractions();
         });
     }
@@ -40,18 +35,13 @@ public class TreeMojoExclusionsTest extends BaseTreeMojoTest {
             inOrder.verify(mockedLog).info("Gathering dependency tree of dependencyManagement section. May take a while");
             inOrder.verify(mockedLog).info("Dependency tree of dependencymanagement configuration:");
             inOrder.verify(mockedLog).info("de.ebp:tree-mojo-test-exclusions-indirect:pom:0.0.1-SNAPSHOT");
-            inOrder.verify(mockedLog).info("\\- com.google.inject:guice:jar:5.1.0:compile");
-            inOrder.verify(mockedLog).info("   +- javax.inject:javax.inject:jar:1:compile");
-            inOrder.verify(mockedLog).info("   +- aopalliance:aopalliance:jar:1.0:compile");
-            inOrder.verify(mockedLog).info("   +- com.google.guava:guava:jar:30.1-jre:compile");
+            inOrder.verify(mockedLog).info("\\- com.google.dagger:dagger-android:jar:2.41:compile");
+            inOrder.verify(mockedLog).info("   +- com.google.dagger:dagger:jar:2.41:compile");
+            inOrder.verify(mockedLog).info("   |  \\- javax.inject:javax.inject:jar:1:compile");
+            inOrder.verify(mockedLog).info("   +- com.google.dagger:dagger-lint-aar:jar:2.41:compile");
+            inOrder.verify(mockedLog).info("   +- androidx.annotation:annotation:jar:1.2.0:compile");
             // this SHOULD be missing
-            // inOrder.verify(mockedLog).info("   |  +- com.google.guava:failureaccess:jar:1.0.1:compile");
-            inOrder.verify(mockedLog).info("   |  +- com.google.guava:listenablefuture:jar:9999.0-empty-to-avoid-conflict-with-guava:compile");
-            inOrder.verify(mockedLog).info("   |  +- com.google.code.findbugs:jsr305:jar:3.0.2:compile");
-            inOrder.verify(mockedLog).info("   |  +- org.checkerframework:checker-qual:jar:3.5.0:compile");
-            inOrder.verify(mockedLog).info("   |  +- com.google.errorprone:error_prone_annotations:jar:2.3.4:compile");
-            inOrder.verify(mockedLog).info("   |  \\- com.google.j2objc:j2objc-annotations:jar:1.3:compile");
-            inOrder.verify(mockedLog).info("   \\- org.ow2.asm:asm:jar:9.2:compile");
+            // inOrder.verify(mockedLog).info("   \\- javax.inject:javax.inject:jar:1:compile");
             inOrder.verifyNoMoreInteractions();
         });
     }
