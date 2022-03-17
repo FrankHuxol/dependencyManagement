92f5087a6ca1dfef7ad67d7c97df73d4069e1459
FrankHuxol <frank.huxol@ebp.de>
2022-03-17 09:33:01 +0100

    fixed some tests according to new ignore of test dependencies

--- src/test/java/de/ebp/dependencymanagement/TreeMojoRestrictScopeTest.java
+++ src/test/java/de/ebp/dependencymanagement/TreeMojoRestrictScopeTest.java
@@ -42,19 +42,10 @@ public class TreeMojoRestrictScopeTest extends BaseTreeMojoTest {
             inOrder.verify(mockedLog).info("|  +- javax.measure:unit-api:jar:2.1.3:compile");
             inOrder.verify(mockedLog).info("|  +- tech.uom.lib:uom-lib-common:jar:2.1:compile");
             inOrder.verify(mockedLog).info("|  +- javax.inject:javax.inject:jar:1:compile");
-            inOrder.verify(mockedLog).info("|  +- org.apiguardian:apiguardian-api:jar:1.1.1:compile");
-            inOrder.verify(mockedLog).info("|  +- org.hamcrest:hamcrest-library:jar:2.2:test");
-            inOrder.verify(mockedLog).info("|  +- org.junit.jupiter:junit-jupiter-api:jar:5.7.1:test");
-            inOrder.verify(mockedLog).info("|  +- org.junit.jupiter:junit-jupiter-params:jar:5.7.1:test");
-            inOrder.verify(mockedLog).info("|  +- org.junit.jupiter:junit-jupiter-engine:jar:5.7.1:test");
-            inOrder.verify(mockedLog).info("|  +- org.junit.platform:junit-platform-launcher:jar:1.7.1:test");
-            inOrder.verify(mockedLog).info("|  \\- org.junit.platform:junit-platform-runner:jar:1.7.1:test");
+            inOrder.verify(mockedLog).info("|  \\- org.apiguardian:apiguardian-api:jar:1.1.1:compile");
             inOrder.verify(mockedLog).info("+- org.slf4j:slf4j-api:jar:1.7.36:compile");
-            inOrder.verify(mockedLog).info("|  \\- junit:junit:jar:4.13:test");
             inOrder.verify(mockedLog).info("\\- junit:junit:jar:4.13.2:test");
-            inOrder.verify(mockedLog).info("   +- org.hamcrest:hamcrest-core:jar:1.3:compile");
-            inOrder.verify(mockedLog).info("   \\- org.hamcrest:hamcrest-library:jar:1.3:test");
-            inOrder.verify(mockedLog).info("      \\- org.hamcrest:hamcrest-core:jar:1.3:compile");
+            inOrder.verify(mockedLog).info("   \\- org.hamcrest:hamcrest-core:jar:1.3:compile");
             inOrder.verifyNoMoreInteractions();
         });
     }
@@ -72,7 +63,6 @@ public class TreeMojoRestrictScopeTest extends BaseTreeMojoTest {
             inOrder.verify(mockedLog).info("Dependency tree of dependencymanagement configuration:");
             inOrder.verify(mockedLog).info("de.ebp:tree-mojo-test-scopeTestOnly:pom:0.0.1-SNAPSHOT");
             inOrder.verify(mockedLog).info("\\- junit:junit:jar:4.13.2:test");
-            inOrder.verify(mockedLog).info("   \\- org.hamcrest:hamcrest-library:jar:1.3:test");
             inOrder.verifyNoMoreInteractions();
         });
     }
